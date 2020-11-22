ba38457be7b4d45a21acaba25bf2686f3709b337
delamey <245510080@qq.com>
2020-11-22 21:56:02 +0800

    修改启动类

--- eureka/src/main/java/eureka/EurekaApplication.java
+++ eureka/src/main/java/eureka/EurekaApplication.java
@@ -2,6 +2,8 @@ package eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,8 +13,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaApplication.class);
     }
 }
